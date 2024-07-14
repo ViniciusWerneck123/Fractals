fd2902eb3809b1f52ea93e82fe4369bf920cdbf3
Vinicius Werneck <viniciusmfwerneck01@gmail.com>
2024-07-15 00:59:35 +0200

    Implemented zoom

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ y: {-1.5, 1.5}
 750x750 matrix of points'''
 a, b = 0.285, 0.01
 c = complex(a, b)
-z, color = fractal(c, n_points=1000, clean_plot=False)
+z, color = fractal(c, n_points=750, clean_plot=False, zoom=15, center_x=-0.39, center_y=-0.30)
 
 
-#z, color = MandelbrotSet(n_points=750, clean_plot=False)
+#z, color = MandelbrotSet(n_points=750, clean_plot=False, zoom=1, center_x=None, center_y=None)
--- functions.py
+++ functions.py
@@ -13,21 +13,39 @@ STOP_STEP = 100
 CMAP = 'viridis'
 INTERIOR_COLOR = [0, 0, 0] # RGB
 GRAPH_WIDTH = 9
+MINIMUM_ITERATIONS = 25
 
-def fractal(c: complex, xlim: tuple = DEFAULT_XLIM, ylim: tuple = DEFAULT_YLIM, n_points: int = N_POINTS, forced_stop=False, stop_step=STOP_STEP, cmap=CMAP,
-            interior_color=INTERIOR_COLOR, clean_plot=True, width=GRAPH_WIDTH) -> Tuple[np.array, np.array, np.array]:
+def fractal(c: complex, n_points: int = N_POINTS, forced_stop=False, stop_step=STOP_STEP, cmap=CMAP,
+            interior_color=INTERIOR_COLOR, clean_plot=True, width=GRAPH_WIDTH,
+            zoom=1, center_x=None, center_y=None) -> Tuple[np.array, np.array, np.array]:
     '''Function that return the points and colors for each point for the fractal.
-    Return a tuple of matrices with x and y values of the mandelbrot set.\n                                                                                                                                                     
-    xlim: tuple with lower and upper limit for x axes.                                                         
-    ylim: tuple with lower and upper limit for y axes.                                                  
+    Return a tuple of matrices with x and y values of the mandelbrot set.\n                                                  
+    c: the complex constant value of the sequence.
     n_points: number of lines and columns in the square grid of points.                                                     
     forced_stop: if the user wants to run the fractal faster but in a incomplete way. Sometimes the fractals take long to calculate, this can be used
     to end the convergence loop before the stop condition.                                                                          
     stop_step: the value of the iteration the user wants to force the calculation to stop.                                                      
     cmap: cmap used for coloring the fractal. Default to viridis.                                                       
-    interior_color: color in RGB of points that converge. Default to black'''
+    interior_color: color in RGB of points that converge. Default to black                                                              
+    clean_plot: if the image is displayed without the axis labels. Default True                                                                 
+    width: width of the figure                                                                              
+    zoom: the amount of zoom                                                                        
+    center_x: x coordinate of the center point of the figure                                                        
+    center_y: y coordinate of the center point of the figure'''
+    
+    if type(center_x) == type(None):
+        dx = (sum(DEFAULT_XLIM))/2 - (sum(DEFAULT_XLIM)/zoom)/2
+    else:
+        dx = center_x - (sum(DEFAULT_XLIM)/zoom)/2
+
+    if type(center_y) == type(None):
+        dy = (sum(DEFAULT_YLIM))/2 - (sum(DEFAULT_YLIM)/zoom)/2
+    else:   
+        dy = center_y - (sum(DEFAULT_YLIM)/zoom)/2
+
     # Grid of points
-    x, y = np.linspace(xlim[0], xlim[1], n_points, dtype=np.float64), np.linspace(ylim[0], ylim[1], n_points, dtype=np.float64)
+    x = np.linspace(DEFAULT_XLIM[0]/zoom + dx, DEFAULT_XLIM[1]/zoom + dx, n_points, dtype=np.float64)
+    y = np.linspace(DEFAULT_YLIM[0]/zoom + dy, DEFAULT_YLIM[1]/zoom + dy, n_points, dtype=np.float64)
     values = product(x, y)
 
     del x, y
@@ -50,7 +68,7 @@ def fractal(c: complex, xlim: tuple = DEFAULT_XLIM, ylim: tuple = DEFAULT_YLIM, 
         diverging = np.absolute(z) > 2
 
         # If there is no point diverging, leave the loop
-        if np.all(~diverging):
+        if np.all(~diverging) and i > MINIMUM_ITERATIONS * zoom:
             break
 
         new_point = color == -1
@@ -110,19 +128,36 @@ def newtonIteration(x, y, a, b):
 
 
 
-def MandelbrotSet(xlim: tuple = MANDELBROT_XLIM, ylim: tuple = MANDELBROT_YLIM, n_points: int = N_POINTS, forced_stop = False, stop_step=STOP_STEP, cmap=CMAP,
-                  interior_color=INTERIOR_COLOR, clean_plot=True, width=GRAPH_WIDTH):
+def MandelbrotSet(n_points: int = N_POINTS, forced_stop = False, stop_step=STOP_STEP, cmap=CMAP,
+                  interior_color=INTERIOR_COLOR, clean_plot=True, width=GRAPH_WIDTH,
+                  zoom=1, center_x=None, center_y=None):
     '''Generate the points and color of the Mandelbrot set                                                      
     Return the values z of the plane and the colors of each point
 
-    xlim/ylim: limits for the points.                                                       
-    n_points: number of points of the n_points x n_points matrix.                                                               
+    n_points: number of lines and columns in the square grid of points.                                                               
     forced_stop: if the user wants to run the fractal faster but in a incomplete way. Sometimes the fractals take long to calculate, this can be used
     to end the convergence loop before the stop condition.                                                                          
     stop_step: the value of the iteration the user wants to force the calculation to stop.                                                  
     cmap: cmap used for coloring the fractal. Default to viridis.                                                   
-    interior_color: color in RGB of points that converge. Default to black'''
-    a, b = np.linspace(xlim[0], xlim[1], n_points, dtype=np.float64), np.linspace(ylim[0], ylim[1], n_points, dtype=np.float64)
+    interior_color: color in RGB of points that converge. Default to black ([0, 0, 0])                                                          
+    clean_plot: if the image is displayed without the axis labels. Default True                                                                 
+    width: width of the figure                                                                              
+    zoom: the amount of zoom                                                                        
+    center_x: x coordinate of the center point of the figure                                                        
+    center_y: y coordinate of the center point of the figure'''
+    # Values of displacement in x and y of the limits of the axes to ensure the new center is in the middle of figure
+    if type(center_x) == type(None):
+        dx = (sum(MANDELBROT_XLIM))/2 - (sum(MANDELBROT_XLIM)/zoom)/2
+    else:
+        dx = center_x - (sum(MANDELBROT_XLIM)/zoom)/2
+
+    if type(center_y) == type(None):
+        dy = (sum(MANDELBROT_YLIM))/2 - (sum(MANDELBROT_YLIM)/zoom)/2
+    else:   
+        dy = center_y - (sum(MANDELBROT_YLIM)/zoom)/2
+    
+    a = np.linspace(MANDELBROT_XLIM[0]/zoom + dx, MANDELBROT_XLIM[1]/zoom + dx, n_points, dtype=np.float64)
+    b = np.linspace(MANDELBROT_YLIM[0]/zoom + dy, MANDELBROT_YLIM[1]/zoom + dy, n_points, dtype=np.float64)
     
     # Complex grid
     c_start = np.array([complex(i[0], i[1]) for i in product(a, b)])
@@ -142,7 +177,7 @@ def MandelbrotSet(xlim: tuple = MANDELBROT_XLIM, ylim: tuple = MANDELBROT_YLIM, 
         diverging = np.absolute(z) > 2
 
         # If there is no point diverging, leave the loop
-        if np.all(~diverging):
+        if np.all(~diverging) and i > MINIMUM_ITERATIONS * zoom:
             break
 
         new_point = color == -1
