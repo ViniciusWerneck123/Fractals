455fe4e3c3273fa48b4e2c7a67f21ca7d82a1b10
Vinicius Werneck <viniciusmfwerneck01@gmail.com>
2024-07-15 23:48:24 +0200

    update test

--- example.py
+++ example.py
@@ -8,9 +8,9 @@ graph limits:
 x: {-1.5, 1.5}
 y: {-1.5, 1.5}
 750x750 matrix of points'''
-a, b = 0.285, 0.01
+a, b = -0.17, 0.8
 c = complex(a, b)
-z, color = juliaSet(c, n_points=750, clean_plot=False, zoom=15, center_x=-0.39, center_y=-0.30)
+z, color = juliaSet(c, n_points=750, clean_plot=False, zoom=1, center_x=None, center_y=None)
 
 
-#z, color = mandelbrotSet(n_points=750, clean_plot=False, zoom=1, center_x=None, center_y=None)
+#z, color = mandelbrotSet(n_points=750, clean_plot=False, cmap='viridis', interior_color=[0, 0, 0], zoom=1, center_x=None, center_y=None)
